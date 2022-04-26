505babfd223dbfecf3a50ee1e0c58d98c7929a8c
Manjit Rabha <demanjt3@outlook.com>
2022-04-26 19:32:37 +0530

    Infinete scroll is added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
-// import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-} from "react-router-dom";
 
 import React, { Component } from 'react'
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
 import News from './components/News';
-// import NewsItem from './components/NewsItem';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
-  pageSize = 15
+  pageSize = 5;
   render() {
     return (
       <div>
-        {/* <Router> */}
-        {/* <Navbar />
-          <News pageSize={this.pageSize} country="in" category="science" /> */}
-        {/* <Switch>
-            <Route path="/"> key="general" <News pageSize={this.pageSize} country="in" category="general" />   </Route>
-            <Route path="/business"> key="business" <News pageSize={this.pageSize} country="in" category="business" />   </Route>
-            <Route path="/entertainment"> key="entertainment" <News pageSize={this.pageSize} country="in" category="entertainment" />   </Route>
-            <Route path="/general"> key="general" <News pageSize={this.pageSize} country="in" category="general" />   </Route>
-            <Route path="/health"> key="health" <News pageSize={this.pageSize} country="in" category="health" />   </Route>
-            <Route path="/science"> key="science" <News pageSize={this.pageSize} country="in" category="science" />   </Route>
-            <Route path="/sports"> key="sports" <News pageSize={this.pageSize} country="in" category="sports" />   </Route>
-            <Route path="/technology"> key="technology" <News pageSize={this.pageSize} country="in" category="technology" />   </Route>
-          </Switch>
-        </Router> */}
         <Router>
           <Navbar />
-          <Routes><Route exact path="/" element={<News pageSize={this.pageSize} country="in" category="general" />} /></Routes>
-          <Routes> <Route exact path='/business' element={<News pageSize={this.pageSize} country="in" category="general" />} />   </Routes>
-          <Routes> <Route exact path='/entertainment' element={<News pageSize={this.pageSize} country="in" category="entertainment" />} />   </Routes>
-          <Routes> <Route exact path='/general' element={<News pageSize={this.pageSize} country="in" category="general" />} />    </Routes>
-          <Routes> <Route exact path='/health' element={<News pageSize={this.pageSize} country="in" category="health" />} />    </Routes>
-          <Routes> <Route exact path='/science' element={<News pageSize={this.pageSize} country="in" category="science" />} />    </Routes>
-          <Routes> <Route exact path='/sports' element={<News pageSize={this.pageSize} country="in" category="sports" />} />    </Routes>
-          <Routes> <Route exact path='/technology' element={<News pageSize={this.pageSize} country="in" category="technology" />} /> </Routes>
+          <Switch>
+            <Route exact path="/"><News key="general" pageSize={this.pageSize} country="in" category="general" /></Route>
+
+            <Route exact path="/business"><News key="business" pageSize={this.pageSize} country="in" category="business" /></Route>
+
+            <Route exact path="/entertainment"><News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" /></Route>
+
+            <Route exact path="/general"><News key="general" pageSize={this.pageSize} country="in" category="general" /></Route>
+
+            <Route exact path="/health"><News key="health" pageSize={this.pageSize} country="in" category="health" /></Route>
+
+            <Route exact path="/science"><News key="science" pageSize={this.pageSize} country="in" category="science" /></Route>
+
+            <Route exact path="/sports"><News key="sports" pageSize={this.pageSize} country="in" category="sports" /></Route>
+
+            <Route exact path="/technology"><News key="technology" pageSize={this.pageSize} country="in" category="technology" /></Route>
+
+          </Switch>
         </Router>
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 
 export class News extends Component {
@@ -84,25 +86,46 @@ export class News extends Component {
     this.setState({ page: this.state.page + 1 })
     this.udateNews()
 
-
   }
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 })
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+      loading: false
+    })
+  };
   render() {
     return (
       <div className="container my-6">
         <h1 className='text-center' style={{ margin: "40px 0px" }}>NewMonkey Top Headlines</h1>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem title={element.title ? element.title.slice(0, 25) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-              </div>)
-          })};
-        </div>
-        <div className="cantainer d-flex justify-content-between my-3">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&laquo; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &raquo;</button>
-        </div>
+        {/* {this.state.loading && <Spinner />} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container" style={{ overflow: 'hidden' }}>
+            <div className="row">
+              {this.state.articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem title={element.title ? element.title.slice(0, 25) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                  </div>)
+              })};
+            </div>
+
+          </div>
+          {/* <div className="cantainer d-flex justify-content-between my-3">
+            <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&laquo; Previous</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &raquo;</button>
+          </div> */}
+        </InfiniteScroll>
       </div>
     )
   }
